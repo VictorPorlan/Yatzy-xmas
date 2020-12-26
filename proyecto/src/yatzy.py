b9b0d3adf9a6cba4764c48608911aaa3c265d82f
VictorPorlan <vporlan@cifpfbmoll.eu>
2020-12-26 16:15:05 +0100

    refactor: :recycle: refactorizadas todas las funciones
    es posible que se puedan mejorar algunas

--- proyecto/src/yatzy.py
+++ proyecto/src/yatzy.py
@@ -49,130 +49,82 @@ class Yatzy:
         return amount * 6
     
     @staticmethod
-    def score_pair( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        at = 0
-        for at in range(6):
-            if (counts[6-at-1] == 2):
-                return (6-at)*2
+    def score_pair(*dices):
+        ordered_dices = sorted(dices, reverse=True)
+        for number in ordered_dices:
+            if ordered_dices.count(number) == 2:
+                return number * 2
         return 0
     
     @staticmethod
-    def two_pairs( d1,  d2,  d3,  d4,  d5):
-        counts = [0]*6
-        counts[d1-1] += 1
-        counts[d2-1] += 1
-        counts[d3-1] += 1
-        counts[d4-1] += 1
-        counts[d5-1] += 1
-        n = 0
-        score = 0
-        for i in range(6):
-            if (counts[6-i-1] >= 2):
-                n = n+1
-                score += (6-i)
-                    
-        if (n == 2):
-            return score * 2
+    def two_pairs(*dices):
+        ordered_dices = sorted(dices, reverse=True)
+        last_number = ""
+        result = 0
+        pairs = 0
+        for number in ordered_dices:
+            if number == last_number:
+                pass
+            elif ordered_dices.count(number) >= 2:
+                result += number * 2
+                pairs += 1
+            last_number = number
+        if pairs == 2:    
+            return result
         else:
             return 0
+
     
     @staticmethod
-    def four_of_a_kind( _1,  _2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[_1-1] += 1
-        tallies[_2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        for i in range(6):
-            if (tallies[i] >= 4):
-                return (i+1) * 4
+    def four_of_a_kind( *dices):
+        ordered_dices = sorted(dices, reverse=True)
+        for number in ordered_dices:
+            if ordered_dices.count(number) >= 4:
+                return number * 4
         return 0
     
 
     @staticmethod
-    def three_of_a_kind( d1,  d2,  d3,  d4,  d5):
-        t = [0]*6
-        t[d1-1] += 1
-        t[d2-1] += 1
-        t[d3-1] += 1
-        t[d4-1] += 1
-        t[d5-1] += 1
-        for i in range(6):
-            if (t[i] >= 3):
-                return (i+1) * 3
+    def three_of_a_kind(*dices):
+        ordered_dices = sorted(dices, reverse=True)
+        for number in ordered_dices:
+            if ordered_dices.count(number) >= 3:
+                return number * 3
         return 0
     
 
     @staticmethod
-    def smallStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[0] == 1 and
-            tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1):
-            return 15
+    def smallStraight(*dices):
+        if sorted(list(dices)) == list(range(1, 6)):
+            return sum(dices)
         return 0
     
 
     @staticmethod
-    def largeStraight( d1,  d2,  d3,  d4,  d5):
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-        if (tallies[1] == 1 and
-            tallies[2] == 1 and
-            tallies[3] == 1 and
-            tallies[4] == 1
-            and tallies[5] == 1):
-            return 20
+    def largeStraight(*dices):
+        if sorted(list(dices)) == list(range(2, 7)):
+            return sum(dices)
         return 0
     
 
     @staticmethod
-    def fullHouse( d1,  d2,  d3,  d4,  d5):
-        tallies = []
-        _2 = False
-        i = 0
-        _2_at = 0
-        _3 = False
-        _3_at = 0
-
-        tallies = [0]*6
-        tallies[d1-1] += 1
-        tallies[d2-1] += 1
-        tallies[d3-1] += 1
-        tallies[d4-1] += 1
-        tallies[d5-1] += 1
-
-        for i in range(6):
-            if (tallies[i] == 2): 
-                _2 = True
-                _2_at = i+1
-            
-
-        for i in range(6):
-            if (tallies[i] == 3): 
-                _3 = True
-                _3_at = i+1
-            
-
-        if (_2 and _3):
-            return _2_at * 2 + _3_at * 3
+    def fullHouse(*dices):
+        result = 0
+        pair = False
+        three_of_a_kind = False
+        ordered_dices = sorted(dices, reverse=True)
+        last_number = ""
+        for number in ordered_dices:
+            if number == last_number:
+                pass
+            elif ordered_dices.count(number) == 2:
+                result += number * 2
+                pair = True
+            elif ordered_dices.count(number) == 3:
+                result += number * 3
+                three_of_a_kind = True
+            last_number = number
+        if pair == True and three_of_a_kind == True:    
+            return result
         else:
             return 0
